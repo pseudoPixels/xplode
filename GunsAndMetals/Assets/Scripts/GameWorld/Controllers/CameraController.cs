0ada9fa0d90873834fc8d88e68bf98e0c71b6348
codevisionProjects <codevision.source@gmail.com>
2016-10-10 00:24:06 -0600

    Designed the basic Game World. Player Horizontal control with walk and idle animation included.

---
+++ GunsAndMetals/Assets/Scripts/GameWorld/Controllers/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+    private PlayerController playerController;
+    private Transform playerTransform;
+
+
+    private Vector3 cameraNewPosition;
+
+
+	// Use this for initialization
+	void Start () {
+        playerController = player.GetComponent<PlayerController>();
+        playerTransform = player.GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(playerController.isPlayerAlive() == true){
+            cameraNewPosition = new Vector3(playerTransform.position.x+3.0f, playerTransform.position.y+3.0f, transform.position.z);
+            transform.position = cameraNewPosition;
+        }
+	}
+}
---
+++ GunsAndMetals/Assets/Scripts/PlayerController/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    //Player Controller Vars
+    private float horizontalInput;
+    private float verticalInput;
+    private bool isFacingRight=true;
+
+
+    //Player Health and Life
+    private int playerMaxHealth = 100;
+    private int playerCurrentHealth = 100;
+    
+
+
+
+    //Animator
+    private Animator anim;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //Get the Keyboard Input
+        horizontalInput =  Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        //Apply the input to the player
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(horizontalInput * Time.deltaTime*4300.0f, 200.0f*verticalInput * Time.deltaTime));
+        if (horizontalInput > 0 && !isFacingRight) playerFlip();
+        else if (horizontalInput < 0 && isFacingRight) playerFlip();
+        anim.SetFloat("Speed", Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x));
+
+    }
+
+    void playerFlip(){
+        isFacingRight = !isFacingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    public bool isPlayerAlive(){
+        if (playerCurrentHealth <= 0) return false;
+        return true;
+    }
+}
